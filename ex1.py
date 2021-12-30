15a42c4a5ed7e6e85224874f691276868cce081b
WANGHanshuo1220 <whs13720400638@163.com>
2021-12-30 12:27:33 +0100

    ex1 test without func code

---
+++ ex1.py
@@ -0,0 +1,27 @@
+
+def cal_moyenne(list_a):
+    return -1
+
+
+def cal_mediane(list_a):
+    return -1
+
+
+def cal_ecart(list_a):
+    return -1
+
+
+def is_geometrique(list_a):
+    return -1
+
+
+def is_arithmetique(list_a):
+    return -1
+
+
+def is_geometrique2(n, list_a):
+    return -1
+
+
+def is_arithmetique2(n, list_a):
+    return -1
---
+++ test_ex1.py
@@ -0,0 +1,55 @@
+import ex1
+import unittest
+
+class test(unittest.TestCase):
+    def test_cal_moyenne(self):
+        self.assertEqual(ex1.cal_moyenne((1, 2, 3, 4, 5)), 15)
+        self.assertEqual(ex1.cal_moyenne((10, 2, 3, 1)), 16)
+        self.assertEqual(ex1.cal_moyenne((1, 2, 3, 4)), 10)
+        self.assertEqual(ex1.cal_moyenne((1, 0, 0, 0)), 1)
+
+
+    def test_cal_mediane(self):
+        self.assertEqual(ex1.cal_mediane((1, 2, 3, 4, 5)), 3)
+        self.assertEqual(ex1.cal_mediane((10, 2, 3, 1)), 2.5)
+        self.assertEqual(ex1.cal_mediane((1, 2, 3, 4, 1, 2)), 2)
+        self.assertEqual(ex1.cal_mediane((1, 0, 0, 0)), 0)
+
+
+    def test_cal_ecart(self):
+        self.assertEqual(ex1.cal_ecart((1, 2, 3, 4, 5)), 10)
+        self.assertEqual(ex1.cal_ecart((10, 2, 3, 1)), 50)
+        self.assertEqual(ex1.cal_ecart((1, 2, 3, 4)), 5)
+        self.assertEqual(ex1.cal_ecart((1, 0, 0, 0)), 0.75)
+
+
+    def test_is_geometrique(self):
+        self.assertEqual(ex1.is_geometrique((1, 2, 4, 8, 16)), True)
+        self.assertEqual(ex1.is_geometrique((10, 2, 3, 1)), False)
+        self.assertEqual(ex1.is_geometrique((2, -2, 2, -2, 2)), True)
+        self.assertEqual(ex1.is_geometrique((1, 0, 0, 0)), False)
+
+
+    def test_is_arithmetique(self):
+        self.assertEqual(ex1.is_arithmetique((1, 2, 3, 4, 5)), True)
+        self.assertEqual(ex1.is_arithmetique((10, 2, 3, 1)), False)
+        self.assertEqual(ex1.is_arithmetique((2, 1, 0, -1, -2)), True)
+        self.assertEqual(ex1.is_arithmetique((1, 0, 0, 0)), False)
+
+
+    def test_is_geometrique2(self):
+        self.assertEqual(ex1.is_geometrique2(3, (1, 2, 4, 8, 16)), (True, (32, 64, 128)))
+        self.assertEqual(ex1.is_geometrique2(1, (10, 2, 3, 1)), False)
+        self.assertEqual(ex1.is_geometrique2(2, (2, -2, 2, -2, 2)), (True, (-2, 2)))
+        self.assertEqual(ex1.is_geometrique2((1, 0, 0, 0)), False)
+
+
+    def test_is_arithmetique2(self):
+        self.assertEqual(ex1.is_arithmetique2(2, (1, 2, 3, 4, 5)), (True, 6, 7))
+        self.assertEqual(ex1.is_arithmetique2(1, (10, 2, 3, 1)), False)
+        self.assertEqual(ex1.is_arithmetique2(3, (2, 1, 0, -1, -2)), (True, (-3, -4, -5)))
+        self.assertEqual(ex1.is_arithmetique2((1, 0, 0, 0)), False)
+
+
+if __name__ == "__main__":
+    unittest.main()
